0202198f16f088af47f4b92c0a47fc9b2a2dc979
rjgoif <rjgoif+github@gmail.com>
2025-12-02 03:12:02 -0500

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,8 @@
+"""
+ComfyUI Img Label Tools
+Custom nodes for label manipulation and text processing in ComfyUI.
+"""
+
+from .nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
